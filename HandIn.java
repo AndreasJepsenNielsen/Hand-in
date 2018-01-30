65808900190403f929f8f365e3ed9569a979fb6b
AndreasJepsenNielsen <32238389+AndreasJepsenNielsen@users.noreply.github.com>
2018-01-30 13:24:58 +0100

    HandIn Første dag

---
+++ HandIn.java
@@ -0,0 +1,14 @@
+public class HandIn
+{
+    public static void main(String[] args)
+    {
+        print10();
+    }
+    public static void print10()
+    {
+        for (int i = 1; i < 11; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
